41a76cbdd5ef88ebe192dcdc99ca08d664751ad8
Alexandru Cobuz <alexandru.cobuz@gmail.com>
2015-06-14 05:45:52 +0300

    fix

--- hackaton/js/scripts.js
+++ hackaton/js/scripts.js
@@ -1,29 +1,31 @@
-$('a[data-toggle="tab"]').on('click', function (e) {
-    var target = $(e.target).attr("href");
+$( document ).ready(function(){
+    $('a[data-toggle="tab"]').on('click', function (e) {
+        var target = $(e.target).attr("href");
 
-    if(target == '#youtube') {
-        $('#description').hide();
-        $('#specialized').hide();
-        $('#clients').hide();
-        $(target).show();
-    }
-    else if (target == '#description') {
-        $('#youtube').hide();
-        $('#specialized').hide();
-        $('#clients').hide();
-        $(target).show();
-    }
-    else if (target == '#specialized') {
-        $('#youtube').hide();
-        $('#description').hide();
-        $('#clients').hide();
-        $(target).show();
-    }
-    else if (target == '#clients') {
-        $('#youtube').hide();
-        $('#description').hide();
-        $('#specialized').hide();
-        $(target).show();
-    }
+        if(target == '#youtube') {
+            $('#description').hide();
+            $('#specialized').hide();
+            $('#clients').hide();
+            $(target).show();
+        }
+        else if (target == '#description') {
+            $('#youtube').hide();
+            $('#specialized').hide();
+            $('#clients').hide();
+            $(target).show();
+        }
+        else if (target == '#specialized') {
+            $('#youtube').hide();
+            $('#description').hide();
+            $('#clients').hide();
+            $(target).show();
+        }
+        else if (target == '#clients') {
+            $('#youtube').hide();
+            $('#description').hide();
+            $('#specialized').hide();
+            $(target).show();
+        }
 
+    });
 });
