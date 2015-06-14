39633159f42b453908cba5a3d5546b36a7a79da4
Alexandru Cobuz <alexandru.cobuz@gmail.com>
2015-06-14 04:15:41 +0300

    fix

--- hackaton/index.php
+++ hackaton/index.php
@@ -1,4 +1,4 @@
-<form action="fileUpload1.php" name="upload" enctype="multipart/form-data">
+<form action="apiRegongition.php?req=image_upload" name="upload" enctype="multipart/form-data">
     <input type="file" name="pic" size="40">
 
     <input type="submit" value="Upload">
